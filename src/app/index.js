14cca22673ae34780b245efe17eb6472871c2f26
Domas Piragas <domas.piragas@gmail.com>
2019-08-17 21:45:54 -0600

    styled-components

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
 const App = props => {
+    console.log('styledz', styled);
   return (
       <div style={{display: 'flex', justifyContent: 'center'}}>
           <img src="src/resources/PageUnderConstruction.png" />
@@ -10,3 +12,8 @@ const App = props => {
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+const ContentWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
