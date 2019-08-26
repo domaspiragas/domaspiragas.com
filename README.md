3b5d3a185446c245b217b8fb6ad948a2f1997f4a
Domas Piragas <domas.piragas@gmail.com>
2019-08-25 22:26:33 -0600

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# domaspiragas.com
-Personal Portfolio
--- src/utils/theme.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-    primary: '#ffffff',
-    secondary: '#000000',
-    primaryText: '#000000',
-    secondaryText: '#ffffff'
-};
-
-export default {colors};
